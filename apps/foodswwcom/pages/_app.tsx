df603229dffca8fec2bd53e787a41a8d189350a6
ricardojrmcom <liber@ricardojrm.com>
2022-12-02 06:57:07 +0000

    feat: init foodswwcom app

---
+++ apps/foodswwcom/pages/_app.tsx
@@ -0,0 +1,29 @@
+import type { AppProps, AppType } from 'next/app';
+import { CacheProvider } from '@emotion/react';
+import type { EmotionCache } from '@emotion/react';
+import { createEmotionCache } from 'scripts';
+import { UiProvider } from 'ui/src/utils';
+
+const clientSideEmotionCache = createEmotionCache();
+
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_DEVDIGITALXYZ || '';
+
+interface UiAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+const UiApp: AppType = (props: UiAppProps) => {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const AnyComponent = Component as any;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <UiProvider GA_MEASUREMENT_ID={GA_MEASUREMENT_ID}>
+        <AnyComponent {...pageProps} />
+      </UiProvider>
+    </CacheProvider>
+  );
+};
+
+export default UiApp;
