ad092ef3bb80b0cfd0a3a6134a69a108c1983db7
ricardojrmcom <liber@ricardojrm.com>
2022-12-20 20:07:13 +0000

    fix(dwa): fix build

--- apps/rjdreamwithart/pages/about.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import type { NextPage } from 'next';
-import { AboutView } from '../views';
-
-const AboutPage: NextPage = () => {
-  return <AboutView />;
-};
-
-export default AboutPage;
--- apps/rjdreamwithart/pages/services.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import type { NextPage } from 'next';
-import { ServicesView } from '../views';
-
-const ServicesPage: NextPage = () => {
-  return <ServicesView />;
-};
-
-export default ServicesPage;
