66a070c950db96b90ea76507085234f59c3fb849
ricardojrmcom <liber@ricardojrm.com>
2023-01-11 05:25:56 +0000

    fix: update text

--- apps/ricardojrmcom/views/Home/index.tsx
+++ apps/ricardojrmcom/views/Home/index.tsx
@@ -31,7 +31,7 @@ export const HomeView = () => {
             </LinkButton>
           </Box>
           <Box textAlign='center' mb={3} mt={1}>
-            <Subtitle1 paragraph>Indie Business Development</Subtitle1>
+            <Subtitle1 paragraph>Indie Business Hacking</Subtitle1>
             <Subtitle2>All about AI and Automation</Subtitle2>
           </Box>
           <Box textAlign='center' mt={1.5} mb={1.5}>
